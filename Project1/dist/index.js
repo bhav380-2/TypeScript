3a98ec5b171ff97236528294c1a74b8ba411b7ab
bhav380 <bhaweshrathour18@gmail.com>
2025-01-04 13:56:05 +0530

    type modifiers in classes

--- Project1/dist/index.js
+++ Project1/dist/index.js
@@ -1,14 +1,50 @@
 "use strict";
 // console.log("Hello typescript is here")
 // console.log("Typescript is amazing")
+// class User{
+//     public email : string
+//     private name: string
+//     // #name : string // this is also private -> js syntax
+//     readonly city : string = "Jaipur"
+//     constructor(email:string,name:string){
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
 class User {
     constructor(email, name) {
-        this.city = "Jaipur";
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
+        this.city = "Jaipur";
+    }
+    deleteToken() {
+        console.log("deleted token");
+    }
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
     }
 }
 const hitesh = new User("h@h.com", "hitesh");
+// hitesh.deleteToken()
 console.log(hitesh.city);
 // hitesh.city = 2
 // hitesh.city = "ntl"
--- Project1/src/index.ts
+++ Project1/src/index.ts
@@ -2,19 +2,54 @@
 // console.log("Typescript is amazing")
 
 
-class User{
+// class User{
+//     public email : string
+//     private name: string
+//     // #name : string // this is also private -> js syntax
+//     readonly city : string = "Jaipur"
+//     constructor(email:string,name:string){
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
 
-    email : string
-    name: string
+
+class User{
+    protected _courseCount = 1
     readonly city : string = "Jaipur"
-    constructor(email:string,name:string){
-        this.email = email;
-        this.name = name
+    constructor(public email:string,public name:string){
+    }
+    private deleteToken(){
+        console.log("deleted token")
+    }
+
+    get getAppleEmail():string{
+        return `apple ${this.email}`
+    }
+
+    get courseCount():number{
+        return this._courseCount
+    }
+
+    set courseCount(courseNum){
+        if(courseNum<=1){
+            throw new Error("course count should be more than 1")
+        }
+        this._courseCount = courseNum 
+    }
+}
+
+
+class SubUser extends User{
+    isFamily : boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
     }
 }
 
 
 const hitesh = new User("h@h.com","hitesh")
+// hitesh.deleteToken()
 
 console.log(hitesh.city)
 // hitesh.city = 2
