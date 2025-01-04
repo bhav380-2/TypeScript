6d79d9a84f246240c6adeb373152f1463a3426df
bhav380 <bhaweshrathour18@gmail.com>
2025-01-04 11:21:17 +0530

    classes

--- Project1/dist/index.js
+++ Project1/dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log("Hello typescript is here");
-console.log("Typescript is amazing");
+// console.log("Hello typescript is here")
+// console.log("Typescript is amazing")
+class User {
+    constructor(email, name) {
+        this.city = "Jaipur";
+        this.email = email;
+        this.name = name;
+    }
+}
+const hitesh = new User("h@h.com", "hitesh");
+console.log(hitesh.city);
+// hitesh.city = 2
+// hitesh.city = "ntl"
--- Project1/src/index.ts
+++ Project1/src/index.ts
@@ -1,2 +1,21 @@
-console.log("Hello typescript is here")
-console.log("Typescript is amazing")
+// console.log("Hello typescript is here")
+// console.log("Typescript is amazing")
+
+
+class User{
+
+    email : string
+    name: string
+    readonly city : string = "Jaipur"
+    constructor(email:string,name:string){
+        this.email = email;
+        this.name = name
+    }
+}
+
+
+const hitesh = new User("h@h.com","hitesh")
+
+console.log(hitesh.city)
+// hitesh.city = 2
+// hitesh.city = "ntl"
