cb41ab9d4e7eb2ec767ec2c4d7b4872f14b43d59
bhav380 <bhaweshrathour18@gmail.com>
2025-01-04 09:04:08 +0530

    arrays, 2darrays, arrays containing userDefind datatype

---
+++ Intro/arrays/array.ts
@@ -0,0 +1,21 @@
+const superHeros: string[]= []
+// const heroPower : number[] = []
+const heroPower : Array<number> = []
+
+
+
+type User = {
+    name : string
+    isActive : boolean
+}
+const allUsers : User[]  = []
+allUsers.push({name :"",isActive : true})
+
+
+const MLModels : number[][] = [
+    [255,255,255],
+    [1,2,3]
+]
+
+superHeros.push("spiderman")
+heroPower.push(2);
