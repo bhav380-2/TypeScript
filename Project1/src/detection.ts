d99ab14b353ced10e2d8138c9db8d82cb9b52757
bhav380 <bhaweshrathour18@gmail.com>
2025-01-04 19:18:34 +0530

    Exhaustive checking and never type

--- Project1/src/detection.ts
+++ Project1/src/detection.ts
@@ -89,16 +89,32 @@ interface Rectangle{
     width: number
 }
 
-type Shape = Circle | Square
+type Shape = Circle | Square | Rectangle
 
 function  getTrueShape(shape:Shape){
     if(shape.kind==="circle"){
-        return Math.PI * shape.radius;
+        return Math.PI * shape.radius**2;
     }
     return shape.side *shape.side;
 }
 
 
+
+// _________Never type and Exhaustiveness Checking____________
+
+function getArea(shape : Shape){
+    switch(shape.kind){
+        case "circle":
+            return Math.PI * shape.radius**2;
+        case "square":
+            return shape.side *shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape:never = shape
+            return _defaultforshape
+    }
+}
 
 
 
